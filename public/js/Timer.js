2801689f1d2d7c64991e4b6d5b0f1ea88adebe6b
Pontus Persson <pontus.alexander@gmail.com>
2017-10-08 12:19:33 +0200

    Fix Timer pause bug.

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,27 +1,55 @@
+const REASON_EXPLICIT = 0;
+const REASON_BLUR = 1;
+
 export default class Timer {
     constructor(deltaTime = 1/60) {
         let accumulatedTime = 0;
-        let lastTime = 0;
+        this.lastTime = null;
+        this.frameId = null;
+        this.stopReason = null;
 
         this.updateProxy = (time) => {
-            accumulatedTime += (time - lastTime) / 1000;
+            if (this.lastTime !== null) {
+                accumulatedTime += (time - this.lastTime) / 1000;
 
-            while (accumulatedTime > deltaTime) {
-                this.update(deltaTime);
-                accumulatedTime -= deltaTime;
+                while (accumulatedTime > deltaTime) {
+                    this.update(deltaTime);
+                    accumulatedTime -= deltaTime;
+                }
             }
 
-            lastTime = time;
+            this.lastTime = time;
 
             this.enqueue();
         }
     }
 
     enqueue() {
-        requestAnimationFrame(this.updateProxy);
+        this.frameId = this.window.requestAnimationFrame(this.updateProxy);
     }
 
     start() {
         this.enqueue();
+    }
+
+    stop() {
+        this.window.cancelAnimationFrame(this.frameId);
+        this.lastTime = null;
+        this.stopReason = REASON_EXPLICIT;
+    }
+
+    listenTo(window) {
+        this.window = window;
+
+        this.window.addEventListener('blur', () => {
+            this.stop();
+            this.stopReason = REASON_BLUR;
+        });
+
+        this.window.addEventListener('focus', () => {
+            if (this.stopReason === REASON_BLUR) {
+                this.start();
+            }
+        });
     }
 }

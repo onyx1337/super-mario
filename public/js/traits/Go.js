6f608ea4a96db7cc8343895b95c777375ed02d31
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 01:01:05 +0200

    Add speed limit.

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -6,11 +6,17 @@ export default class Go extends Trait {
 
         this.dir = 0;
         this.acceleration = 400;
+        this.speedLimit = 200;
         this.distance = 0;
     }
 
     update(entity, deltaTime) {
         entity.vel.x += this.acceleration * deltaTime * this.dir;
+        if (entity.vel.x > this.speedLimit) {
+            entity.vel.x = this.speedLimit;
+        } else if (entity.vel.x < -this.speedLimit) {
+            entity.vel.x = -this.speedLimit;
+        }
 
         if (this.dir === 0) {
             this.distance = 0;

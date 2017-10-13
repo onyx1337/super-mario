4f6ca1dbe17970b1968eb5344206ab3fd6c1d173
Pontus Persson <pontus.alexander@gmail.com>
2017-10-13 22:47:09 +0200

    Add distance prop to Go trait.

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -6,9 +6,15 @@ export default class Go extends Trait {
 
         this.dir = 0;
         this.speed = 6000;
+        this.distance = 0;
     }
 
     update(entity, deltaTime) {
         entity.vel.x = this.speed * this.dir * deltaTime;
+        if (this.dir === 0) {
+            this.distance = 0;
+        } else {
+            this.distance += Math.abs(entity.vel.x) * deltaTime;
+        }
     }
 }

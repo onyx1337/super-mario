19a47200c4459bc5469dad195872a97cbf831e31
Pontus Persson <pontus.alexander@gmail.com>
2017-10-14 10:11:35 +0200

    Turn off mouse controls.

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,6 @@ import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
 import {createCollisionLayer, createCameraLayer} from './layers.js';
 import {setupKeyboard} from './input.js';
-import {setupMouseControl} from './debug.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -28,8 +27,6 @@ Promise.all([
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
-
-    setupMouseControl(canvas, mario, camera);
 
 
     const timer = new Timer(1/60);
